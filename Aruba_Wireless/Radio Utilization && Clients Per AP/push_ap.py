271b18d84e018b12bb6959986ca81eed235bb067
allenshi86 <zhenning.shi@163.com>
2020-08-10 16:12:15 +0800

    Update push_ap.py

--- Aruba_Wireless/Radio Utilization && Clients Per AP/push_ap.py
+++ Aruba_Wireless/Radio Utilization && Clients Per AP/push_ap.py
@@ -8,7 +8,7 @@ import  json
 ap_json = {"data": []}
 
 
-ap_sets = os.popen("snmpwalk  -v 2c -c momo 172.16.202.10 .1.3.6.1.4.1.14823.2.2.1.5.2.1.4 | grep -i ap | awk -F ':' '{print $4}'|awk -F '\"' '{print $2}'")
+ap_sets = os.popen("snmpwalk  -v 2c -c hello 172.16.202.10 .1.3.6.1.4.1.14823.2.2.1.5.2.1.4 | grep -i ap | awk -F ':' '{print $4}'|awk -F '\"' '{print $2}'")
 ap_list = ap_sets.read().splitlines()
 ap_sets.close()
 
@@ -17,4 +17,4 @@ for ap in ap_list:
 
 push_data = json.dumps(ap_json)
 
-os.system("/bin/zabbix_sender -z 172.16.7.20 -p 10051 -s office-aruba-bj-t2-11-ac-10 -k ap.discovery -o '%s'"  %push_data)
+os.system("/bin/zabbix_sender -z ZABBIX_SERVER -p 10051 -s aruba-bj-ac-10 -k ap.discovery -o '%s'"  %push_data)
