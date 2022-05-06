c286417307270485d37cc893d30095d5b6c4d97b
allenshi86 <zhenning.shi@163.com>
2022-05-06 16:16:50 +0800

    Update push_data_of_ap_cisco.py

--- CISCO Wireless/push_data_of_ap_cisco.py
+++ CISCO Wireless/push_data_of_ap_cisco.py
@@ -9,7 +9,7 @@ prefix_radio_clients_oid = '1.3.6.1.4.1.14179.2.2.2.1.15'
 prefix_channel_oid = '1.3.6.1.4.1.14179.2.2.13.1.3'
 prefix_ap_admin_status_oid = '1.3.6.1.4.1.9.9.513.1.1.1.1.38'
 ap_names_dict = {}   # key存储AP名字，值存储AP索引
-snmpwalk_ap_datas = os.popen("snmpwalk -v 2c -c momo 172.16.201.3 %s" % prefix_ap_oid)
+snmpwalk_ap_datas = os.popen("snmpwalk -v 2c -c mx 172.16.201.x %s" % prefix_ap_oid)
 
 
 def get_ap_dic_fun(dict):
@@ -22,36 +22,36 @@ def get_ap_dic_fun(dict):
 
 def trapper_ap_admin_status(dict_ap):
     for ap_name in dict_ap.keys():
-        snmpwalk_admin_status = os.popen("snmpwalk  -v 2c -c momo 172.16.201.3 %s%s" % (prefix_ap_admin_status_oid, dict_ap[ap_name]))
+        snmpwalk_admin_status = os.popen("snmpwalk  -v 2c -c momo 172.16.201.x %s%s" % (prefix_ap_admin_status_oid, dict_ap[ap_name]))
         ap_status = int(snmpwalk_admin_status.read().split(':')[-1])
-        os.system("/bin/zabbix_sender -z 172.16.7.20 -vv -s office-wlc-bj-t2-11-ac-3 -k status.[%s] -o %s" % (ap_name, ap_status))
+        os.system("/bin/zabbix_sender -z 172.16.7.x -vv -s office-wlc-bj-t2-11-ac-3 -k status.[%s] -o %s" % (ap_name, ap_status))
 
 def trapper_channel_util(dict_ap):
     for ap_name in dict_ap.keys():
         try:
-            snmpwalk_ap_radio0 = os.popen("snmpwalk  -v 2c -c momo 172.16.201.3 %s%s.0" % (prefix_channel_oid, dict_ap[ap_name]))
-            snmpwalk_ap_radio1 = os.popen("snmpwalk  -v 2c -c momo 172.16.201.3 %s%s.1" % (prefix_channel_oid, dict_ap[ap_name]))
+            snmpwalk_ap_radio0 = os.popen("snmpwalk  -v 2c -c momo 172.16.201.x %s%s.0" % (prefix_channel_oid, dict_ap[ap_name]))
+            snmpwalk_ap_radio1 = os.popen("snmpwalk  -v 2c -c momo 172.16.201.x %s%s.1" % (prefix_channel_oid, dict_ap[ap_name]))
             channel0_data_source = snmpwalk_ap_radio0.read()
             channel1_data_source = snmpwalk_ap_radio1.read()
             radio0_util_percent = int(channel0_data_source.split(':')[-1])
             radio1_util_percent = int(channel1_data_source.split(':')[-1])
 #           print radio0_util_percent
 #           print type(radio0_util_percent)
-            os.system("/bin/zabbix_sender -z 172.16.7.20 -vv -s office-wlc-bj-t2-11-ac-3 -k uti_radio0.[%s] -o %s" % (ap_name, radio0_util_percent))
-            os.system("/bin/zabbix_sender -z 172.16.7.20 -vv -s office-wlc-bj-t2-11-ac-3 -k uti_radio1.[%s] -o %s" % (ap_name, radio1_util_percent))
+            os.system("/bin/zabbix_sender -z 172.16.7.x -vv -s office-wlc-bj-t2-11-ac-3 -k uti_radio0.[%s] -o %s" % (ap_name, radio0_util_percent))
+            os.system("/bin/zabbix_sender -z 172.16.7.x -vv -s office-wlc-bj-t2-11-ac-3 -k uti_radio1.[%s] -o %s" % (ap_name, radio1_util_percent))
         except Exception as e:
             print e
 
 def trapper_clients_ap(dict_ap):
     for ap_name in dict_ap.keys():
-        radio0_datas = os.popen("snmpwalk -v 2c -c momo 172.16.201.3 %s%s.2" % (prefix_radio_clients_oid, dict_ap[ap_name])).read()
-        radio1_datas = os.popen("snmpwalk -v 2c -c momo 172.16.201.3 %s%s.1" % (prefix_radio_clients_oid, dict_ap[ap_name])).read()
+        radio0_datas = os.popen("snmpwalk -v 2c -c momo 172.16.201.x %s%s.2" % (prefix_radio_clients_oid, dict_ap[ap_name])).read()
+        radio1_datas = os.popen("snmpwalk -v 2c -c momo 172.16.201.x %s%s.1" % (prefix_radio_clients_oid, dict_ap[ap_name])).read()
         clients_radio0 = int(radio0_datas.split(':')[-1])
         clients_radio1 = int(radio1_datas.split(':')[-1])
         clients_sum = clients_radio0 + clients_radio1
-        os.system("/bin/zabbix_sender -z 172.16.7.20 -vv -s office-wlc-bj-t2-11-ac-3 -k clients.radio0.[%s] -o %s" % (ap_name, clients_radio0))
-        os.system("/bin/zabbix_sender -z 172.16.7.20 -vv -s office-wlc-bj-t2-11-ac-3 -k clients.radio1.[%s] -o %s" % (ap_name, clients_radio1))
-        os.system("/bin/zabbix_sender -z 172.16.7.20 -vv -s office-wlc-bj-t2-11-ac-3 -k clients.[%s] -o %s" %(ap_name,clients_sum))
+        os.system("/bin/zabbix_sender -z 172.16.7.x -vv -s office-wlc-bj-t2-11-ac-3 -k clients.radio0.[%s] -o %s" % (ap_name, clients_radio0))
+        os.system("/bin/zabbix_sender -z 172.16.7.x -vv -s office-wlc-bj-t2-11-ac-3 -k clients.radio1.[%s] -o %s" % (ap_name, clients_radio1))
+        os.system("/bin/zabbix_sender -z 172.16.7.x -vv -s office-wlc-bj-t2-11-ac-3 -k clients.[%s] -o %s" %(ap_name,clients_sum))
 
 if __name__ == '__main__':
     get_ap_dic_fun(ap_names_dict)
