e51fde9249ba669a28785082772ec0ec9f2f890d
allenshi86 <zhenning.shi@163.com>
2023-10-09 19:25:36 +0800

    Update Storcli_VDisk_Discovery.ps1

--- RAID_Strocli/Storcli_VDisk_Discovery.ps1
+++ RAID_Strocli/Storcli_VDisk_Discovery.ps1
@@ -1,5 +1,19 @@
 #author:shizhenning
-#----20230921V001----#
+#----20231009V001----#
+
+$Path_tool =  "C:\Program Files\MegaCli\"
+cd $Path_tool
+
+$Count_Physical_Disk =  (.\storcli64.exe /c0  show |findstr "^Virtual" | Out-String).Split("=")[-1]      #获取虚拟磁盘总数量
+$VDisk_Discovery =  .\storcli64.exe /c0  show J nolog 
+$Data_Source = $VDisk_Discovery | ConvertFrom-Json  #json转换成复的自定义对象
+$Raid_Info = $Data_Source.Controllers[0]  
+$Number_VDisk = $Raid_Info.'Response Data'.'VD LIST'.'DG/VD' #获取虚拟磁盘名称,形如0/0,1/1
+
+### 构造json格式
+
+#author:shizhenning
+#----20231009V001----#
 
 $Path_tool =  "C:\Program Files\MegaCli\"
 cd $Path_tool
@@ -22,20 +36,29 @@ $VAR6="},"
 Write-Output $VAR1
 Write-Output "   $VAR2"
 
-ForEach($i in $Number_VDisk)
-{
-Write-Output "      $VAR1"
-Write-Output "         $VAR3 `"$i`""
-    if ($i -eq $Number_VDisk[-1])
-    {         
-        Write-Output "      $VAR5"
-    }
-    else
-      {         
-        Write-Output "      $VAR6"
-    }
+if ($Number_VDisk -is "System.Array") {
+   ForEach($i in $Number_VDisk){
+     Write-Output "      $VAR1"
+     Write-Output "         $VAR3 `"$i`""
+       if ($i -eq $Number_VDisk[-1])
+       {         
+          Write-Output "      $VAR5"
+       }
+       else
+       {         
+          Write-Output "      $VAR6"
+       }
+  }
 }
+else 
+{
+     Write-Output "      $VAR1"
+     Write-Output "         $VAR3 `"$Number_VDisk`""
+     Write-Output "      $VAR5"
+ }
+
 
 Write-Output "   $VAR4"
 Write-Output $VAR5
+
 
