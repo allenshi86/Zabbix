f1441c1611594f5cb7930a1873fb809bf99add5d
allenshi86 <zhenning.shi@163.com>
2023-10-10 16:29:01 +0800

    Update Storcli_VDisk_Info.ps1

--- RAID_Strocli/Storcli_VDisk_Info.ps1
+++ RAID_Strocli/Storcli_VDisk_Info.ps1
@@ -4,12 +4,46 @@
 $Path_tool =  "C:\Program Files\MegaCli\"
 cd $Path_tool
 
+$Count_Physical_Disk =  (.\storcli64.exe /c0  show |findstr "^Virtual" | Out-String).Split("=")[-1]      #获取虚拟磁盘总数量
+$VDisk_Discovery =  .\storcli64.exe /c0  show J nolog 
+$Data_Source = $VDisk_Discovery | ConvertFrom-Json  #json转换成复的自定义对象
+$Raid_Info = $Data_Source.Controllers[0]  
+$Number_VDisk = $Raid_Info.'Response Data'.'VD LIST'.'DG/VD' #获取虚拟磁盘名称,形如0/0,1/1
 
-$No_VDisk = $args[0]  #参数为获取虚拟磁盘名称,形如0/0,1/1
-$No_VDisk = $No_VDisk.split("/")[-1]
-$Attr_VDisk_Query = $args[1]  #参数选项name,size,type,state,cache,scc等 
+### 构造json格式
 
-$Attr_VDisk = .\storcli64.exe /c0  /v$No_VDisk  show J  NOLOG
-$Attr_Source = $Attr_VDisk | ConvertFrom-Json  #获取虚拟磁盘属性列表,比如name,size,type,state,cache,scc等
+$VAR1="{"
+$VAR2='"data": ['
+$VAR3='"{#VDISK}":'
+$VAR4="]"
+$VAR5="}"
+$VAR6="},"
 
-Write-Output  $Attr_Source.Controllers[0].'Response Data'.'Virtual Drives'.$Attr_VDisk_Query
+Write-Output $VAR1
+Write-Output "   $VAR2"
+
+if ($Number_VDisk -is "System.Array") {
+   ForEach($i in $Number_VDisk){
+     Write-Output "      $VAR1"
+     Write-Output "         $VAR3 `"$i`""
+       if ($i -eq $Number_VDisk[-1])
+       {         
+          Write-Output "      $VAR5"
+       }
+       else
+       {         
+          Write-Output "      $VAR6"
+       }
+  }
+}
+else 
+{
+     Write-Output "      $VAR1"
+     Write-Output "         $VAR3 `"$Number_VDisk`""
+     Write-Output "      $VAR5"
+ }
+
+
+Write-Output "   $VAR4"
+Write-Output $VAR5
+
