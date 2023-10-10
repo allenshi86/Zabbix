4cc63b2368c131431499b8864c8a7e446754b34d
allenshi86 <zhenning.shi@163.com>
2023-10-10 16:29:37 +0800

    Update Storcli_VDisk_Info.ps1

--- RAID_Strocli/Storcli_VDisk_Info.ps1
+++ RAID_Strocli/Storcli_VDisk_Info.ps1
@@ -1,49 +1,16 @@
 #author:shizhenning
-#----20230921V001----#
+#----20231010V004----#
 
 $Path_tool =  "C:\Program Files\MegaCli\"
 cd $Path_tool
 
-$Count_Physical_Disk =  (.\storcli64.exe /c0  show |findstr "^Virtual" | Out-String).Split("=")[-1]      #获取虚拟磁盘总数量
-$VDisk_Discovery =  .\storcli64.exe /c0  show J nolog 
-$Data_Source = $VDisk_Discovery | ConvertFrom-Json  #json转换成复的自定义对象
-$Raid_Info = $Data_Source.Controllers[0]  
-$Number_VDisk = $Raid_Info.'Response Data'.'VD LIST'.'DG/VD' #获取虚拟磁盘名称,形如0/0,1/1
+$Location_Physical_Disk = $args[0]       #参数格式[32:6],即磁盘盘位
+$Attr_PDisk = $args[1]                   #参数选项 State, Size ,Interface(SAS\SATA),Model，
 
-### 构造json格式
+$Number_Enclousure = $Location_Physical_Disk.split(":")[0].Split("[")[1]        #获取Enclousure ID,比如32  
+$DID_Physical_Disk = $Location_Physical_Disk.split(":")[1].Split("]")[0]    #从形如[32:2]格式的盘位获取磁盘ID 2,供storcli /s调用
+$PDAttr_info = .\storcli64.exe /c0 /e $Number_Enclousure /s $DID_Physical_Disk show  NOLOG J
 
-$VAR1="{"
-$VAR2='"data": ['
-$VAR3='"{#VDISK}":'
-$VAR4="]"
-$VAR5="}"
-$VAR6="},"
+$Attr_Source = $PDAttr_info | ConvertFrom-Json                             #获取磁盘属性列表,比如State, Size ,Interface(SAS\SATA),Model等
 
-Write-Output $VAR1
-Write-Output "   $VAR2"
-
-if ($Number_VDisk -is "System.Array") {
-   ForEach($i in $Number_VDisk){
-     Write-Output "      $VAR1"
-     Write-Output "         $VAR3 `"$i`""
-       if ($i -eq $Number_VDisk[-1])
-       {         
-          Write-Output "      $VAR5"
-       }
-       else
-       {         
-          Write-Output "      $VAR6"
-       }
-  }
-}
-else 
-{
-     Write-Output "      $VAR1"
-     Write-Output "         $VAR3 `"$Number_VDisk`""
-     Write-Output "      $VAR5"
- }
-
-
-Write-Output "   $VAR4"
-Write-Output $VAR5
-
+Write-Output  $Attr_Source.Controllers[0].'Response Data'.'Drive Information'.$Attr_PDisk 
