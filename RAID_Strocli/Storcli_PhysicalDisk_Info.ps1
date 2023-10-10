32e53af428cb6262d636966baf395b723d60167a
allenshi86 <zhenning.shi@163.com>
2023-10-10 18:50:06 +0800

    Update Storcli_PhysicalDisk_Info.ps1

--- RAID_Strocli/Storcli_PhysicalDisk_Info.ps1
+++ RAID_Strocli/Storcli_PhysicalDisk_Info.ps1
@@ -7,7 +7,7 @@ cd $Path_tool
 $Location_Physical_Disk = $args[0]       #参数格式[32:6],即磁盘盘位
 $Attr_PDisk = $args[1]                   #参数选项 State, Size ,Interface(SAS\SATA),Model，
 
-$Number_Enclousure = $Location_Physical_Disk.split(":")[0].Split("[")[1]        #获取Enclousure ID,比如32  
+$Number_Enclousure = $Location_Physical_Disk.split(":")[0].Split("[")[-1]        #获取Enclousure ID,比如32  
 $DID_Physical_Disk = $Location_Physical_Disk.split(":")[1].Split("]")[0]    #从形如[32:2]格式的盘位获取磁盘ID 2,供storcli /s调用
 $PDAttr_info = .\storcli64.exe /c0 /e $Number_Enclousure /s $DID_Physical_Disk show  NOLOG J
 
